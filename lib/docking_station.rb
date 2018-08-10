aed8e7cd31c4b9d2babb632dc5660931f3d66317
Andrew Gibbs <asg1968@hotmail.co.uk>
2018-08-10 13:59:13 +0100

    minor changes to docking_station and spec

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -1,17 +1,21 @@
 require_relative "bike"
 
 class DockingStation
-  attr_reader :show_bike
+  attr_reader :bike_array
+
+  def initialize
+    @bike_array =[]
+  end
 
   def release_bike
   #  Bike.new
-  raise "No bikes available" unless @show_bike
-  @show_bike
+  raise "No bikes available" if @bike_array.empty?
+  @bike_array.pop
   end
 
   def dock(bike)
-    raise "Docking station full" unless @show_bike == nil
-    @show_bike = bike
+    raise "Docking station full" if @bike_array.count >= 20
+    @bike_array << bike
   end
 
 #  def show_bike
--- spec/DockingStation_spec.rb
+++ spec/DockingStation_spec.rb
@@ -2,9 +2,9 @@ require "docking_station"
 require "bike"
 describe DockingStation do
 
-  it "releases a bike" do
-    expect(subject).to respond_to :release_bike
-  end
+  # it "releases a bike" do
+  #   expect(subject).to respond_to :release_bike
+  # end
 
   # it "release bike gets a working bike" do
   #   bike = subject.release_bike
@@ -12,28 +12,32 @@ describe DockingStation do
   # end
 
 # Write a unit test for docking a bike at a docking station
-  it "returns bike when dock(bike) called" do
-    bike = Bike.new
-    expect(subject.dock(bike)).to eq bike
-  end
+  # it "returns bike when dock(bike) called" do
+  #   bike = Bike.new
+  #   expect(subject.dock(bike)).to eq bike
+  # end
 
-  it "returns docked bike when show_bike" do
+  it "returns bike_array when we call bike_array" do
     bike = Bike.new
     subject.dock(bike)
-    expect(subject.show_bike).to eq bike
+    expect(subject.bike_array).to include(bike)
   end
 
   describe "#release_bike" do
     it "returns an error when try to release_bike from empty docking station" do
       expect { subject.release_bike }.to raise_error "No bikes available"
     end
+
+    # want to write a test that verifies you can release a bike
   end
 
   describe "#dock(bike)" do
-    it "returns an error when try to dock bike in full docking station" do
-      bike = Bike.new
-      subject.dock(bike)
-      expect { subject.dock(bike) }.to raise_error "Docking station full"
+    it "raises an error when full" do
+      # bike = Bike.new # not needed superceded byline below
+      20.times { subject.dock Bike.new }
+      expect { subject.dock(Bike.new) }.to raise_error "Docking station full"
     end
+
+    # Want to add test that checks the size of the bike_array when we call is n times
   end
 end
